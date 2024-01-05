64d13b2ab126ae91c5f97dcb4ffa83167962a256
limbunhov <141569610+limbunhov@users.noreply.github.com>
2024-01-05 12:44:06 +0700

    Update server.js
    Signed-off-by: limbunhov <141569610+limbunhov@users.noreply.github.com>

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(express.json());
 // app.use(cors());
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:5174'); // Allow requests from any origin
+  res.header('Access-Control-Allow-Origin', '*'); // Allow requests from any origin
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
