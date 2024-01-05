0c050983f088cf0f71c323bda06a7398104553ca
limbunhov <141569610+limbunhov@users.noreply.github.com>
2024-01-05 12:51:50 +0700

    Update server.js
    Signed-off-by: limbunhov <141569610+limbunhov@users.noreply.github.com>

--- server.js
+++ server.js
@@ -20,13 +20,13 @@ app.use(bodyParser.json());
 app.use(express.json());
 
 
-// app.use(cors());
+app.use(cors());
 
-app.use(cors({
-  origin: 'http://localhost:5174',
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true,
-}));
+// app.use(cors({
+//   origin: 'http://localhost:5174',
+//   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+//   credentials: true,
+// }));
 
 
 
